c1b46743dfd1a222f9185b44ec29fcf368f53dc6
Shiraz Buksh <shiraz19@live.com>
2023-05-04 01:50:43 +1200

    project styling updated

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import SubMinder from '../assets/subminder.png'
 const Work = () => {
   return (
     <div name="work" className="w-full md:h-screen text-gray-300 bg-[#0a192f]">
@@ -11,7 +11,19 @@ const Work = () => {
           <p className="py-6">Check out some of my recent projects!</p>
         </div>
 
-        <div className="grid grid-cols-2 gap-20"></div>
+        <div className="w-full flex gap-4 items-center">
+          <img className="w-40" src={SubMinder} alt="HTML icon" />
+          <p className="">
+            {' '}
+            SubMinder is a project I worked on with 5 other developers for our
+            final project during our last week at Dev Academy. This app allows
+            you to track any subscriptions you have recurring such as netflix,
+            spotify etc. Something new I learned during this project was setting
+            up the email reminders. Other features we added include Auth0 for
+            our secure login system, ability to view past/current payments, and
+            the ability to add, update and delete subscriptions.
+          </p>
+        </div>
       </div>
     </div>
   )
