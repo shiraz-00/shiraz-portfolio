b91ccaba6838fd8d8b42d03c3c474770069566ff
Shiraz Buksh <shiraz19@live.com>
2023-05-04 05:18:49 +1200

    ready for deployment

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -100,7 +100,7 @@ const Navbar = () => {
           <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69]">
             <a
               className="flex justify-between items-center w-full text-gray-300"
-              href="/"
+              href="https://drive.google.com/file/d/1x52fbCG-r2t55GvJx68ibxuket3-YPLn/view?usp=sharing"
             >
               Resume <BsFillPersonLinesFill size={30} />
             </a>
